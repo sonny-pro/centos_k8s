5d3cb6f3859a50153114ca39d5fb10b72d8b997a
Sonny Gale <me@sonny.pro>
2020-01-15 20:37:27 -0600

    update certificate script

--- mkCerts.sh
+++ mkCerts.sh
@@ -7,8 +7,11 @@ rm -f ca.pem ca-key.pem cert.pem cert.csr key.pem server-cert.pem server-cert.cs
 openssl genrsa -aes256 -out csr/ca-key.pem 4096
 openssl req -new -x509 -days 365 -key csr/ca-key.pem -sha256 -out csr/ca.pem -config csr/ca.cfg
 
-openssl req -newkey rsa:4096 -nodes -keyout csr/server-key.pem -out csr/server-cert.csr
-openssl req -newkey rsa:4096 -nodes -keyout csr/key.pem -out csr/cert.csr
+openssl genrsa -out csr/server-key.pem 4096
+openssl genrsa -out csr/key.pem 4096
 
-openssl x509 -req -days 365 -sha256 -in csr/server-cert.csr -CA csr/ca.pem -CAkey csr/ca-key.pem -CAcreateserial -out csr/server-cert.pem -extfile csr/server-cert.cfg
-openssl x509 -req -days 365 -sha256 -in csr/cert.csr -CA csr/ca.pem -CAkey csr/ca-key.pem -CAcreateserial -out csr/cert.pem -extfile csr/cert.cfg
+openssl req -sha256 -new -key csr/server-key.pem -out csr/server-cert.csr -config csr/server-cert.cfg
+openssl req -sha256 -new -key csr/key.pem -out csr/cert.csr -config csr/cert.cfg
+
+openssl x509 -req -days 365 -sha256 -in csr/server-cert.csr -CA csr/ca.pem -CAkey csr/ca-key.pem -CAcreateserial -out csr/server-cert.pem
+openssl x509 -req -days 365 -sha256 -in csr/cert.csr -CA csr/ca.pem -CAkey csr/ca-key.pem -CAcreateserial -out csr/cert.pem
