98da8679c9f382a85134409cb7b9101c028d90cd
Sonny Gale <me@sonny.pro>
2020-01-15 20:16:33 -0600

    update certificate script

--- mkCerts.sh
+++ mkCerts.sh
@@ -10,5 +10,5 @@ openssl req -new -x509 -days 365 -key csr/ca-key.pem -sha256 -out csr/ca.pem -co
 openssl req -newkey rsa:4096 -nodes -keyout csr/server-key.pem -out csr/server-cert.csr -config csr/server-cert.cfg
 openssl req -newkey rsa:4096 -nodes -keyout csr/key.pem -out csr/cert.csr -config csr/cert.cfg
 
-openssl x509 -req -days 365 -sha256 -in csr/server-cert.csr -CA csr/ca.pem -CAkey csr/ca-key.pem -CAcreateserial -out csr/server-cert.pem -extfile server-cert.cfg
-openssl x509 -req -days 365 -sha256 -in csr/cert.csr -CA csr/ca.pem -CAkey csr/ca-key.pem -CAcreateserial -out csr/cert.pem -extfile cert.cfg
+openssl x509 -req -days 365 -sha256 -in csr/server-cert.csr -CA csr/ca.pem -CAkey csr/ca-key.pem -CAcreateserial -out csr/server-cert.pem -extfile csr/server-cert.cfg
+openssl x509 -req -days 365 -sha256 -in csr/cert.csr -CA csr/ca.pem -CAkey csr/ca-key.pem -CAcreateserial -out csr/cert.pem -extfile csr/cert.cfg
